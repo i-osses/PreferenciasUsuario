a66402df329aafa07959f48c276a2d3fb520505b
Ignacio Osses <ignacio.osses@gmail.com>
2020-08-17 14:26:25 -0400

    Initial commit

---
+++ app/src/main/java/cl/desafiolatam/desafiounobase/MainActivity.kt
@@ -0,0 +1,47 @@
+package cl.desafiolatam.desafiounobase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    lateinit var nameInput: TextInputEditText
+    lateinit var advance: Button
+    lateinit var container: ConstraintLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nameInput = findViewById(R.id.name_input)
+        advance = findViewById(R.id.login_button)
+        container = findViewById(R.id.container)
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        advance.setOnClickListener {
+            if (nameInput.text!!.isNotEmpty()) {
+                val intent: Intent
+                if (hasSeenWelcome()) {
+                    intent = Intent(this, HomeActivity::class.java)
+                } else {
+                    intent = Intent(this, WelcomeActivity::class.java)
+                }
+                startActivity(intent)
+            } else {
+                Snackbar.make(container, "El nombre no puede estar vacío", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun hasSeenWelcome(): Boolean {
+        var returnValue = false
+        //implementar este método para saber si el usuario ya ha entrado a la aplicación y ha visto
+        //la pantalla de bienvenida. Este método permite decidir que pantalla se muestra después de presionar Ingresar
+        //recorra la lista de usuarios
+        return returnValue
+    }
+}
